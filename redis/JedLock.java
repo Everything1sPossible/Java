bc3a7042d5149be80f90d2389860f1db560da5f3
Everything1sPossible <s563149980@163.com>
2018-11-14 19:43:50 +0800

    redis分布式锁示例

--- redis/JedLock.java
+++ redis/JedLock.java
@@ -2,6 +2,10 @@ package com.sjh.demo;
 
 import redis.clients.jedis.Jedis;
 
+import java.util.Collections;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 /**
  * @author Admin
  * @Description: redis分布式锁
@@ -18,12 +22,16 @@ public class JedLock {
     private boolean locked;
     private long lockValue;
 
-    public synchronized boolean lock(Jedis jedis) {
+    private static final Long RELEASE_SUCCESS = 1L;
+    private static final String LOCK_SUCCESS = "OK";
+
+
+    public boolean lock(Jedis jedis) {
         int retryTime = RETRY_TIME;
         try {
             while (retryTime > 0) {
                 lockValue = System.nanoTime();
-                if ("OK".equalsIgnoreCase(jedis.set(REDIS_LOCK_KEY, String.valueOf(lockValue), "NX", "PX", EXPIRE_TIME))) {
+                if (LOCK_SUCCESS.equalsIgnoreCase(jedis.set(REDIS_LOCK_KEY, String.valueOf(lockValue), "NX", "PX", EXPIRE_TIME))) {
                     locked = true;
                     return locked;
                 }
@@ -36,7 +44,7 @@ public class JedLock {
         return false;
     }
 
-    public synchronized void unlock(Jedis jedis){
+    public void unlock(Jedis jedis){
         if(locked) {
             String currLockVal = jedis.get(REDIS_LOCK_KEY);
             if(currLockVal!=null && Long.valueOf(currLockVal) == lockValue){
@@ -45,24 +53,36 @@ public class JedLock {
             }
         }
     }
+    public boolean unlock2(Jedis jedis){
+        if(locked) {
+            String script = "if redis.call('get', KEYS[1]) == ARGV[1] then return redis.call('del', KEYS[1]) else return 0 end";
+            Object result = jedis.eval(script, Collections.singletonList(REDIS_LOCK_KEY), Collections.singletonList(String.valueOf(lockValue)));
+            if (RELEASE_SUCCESS.equals(result)) {
+                return true;
+            }
+            return false;
+        }
+        return false;
+    }
 
     public static void main(String[] args) {
-        Jedis jedis = new Jedis(REDIS_IP, Integer.valueOf(REDIS_PORT));
-        jedis.auth("123456");
-        JedLock redLock = new JedLock();
         for (int i = 0; i < 100; i++) {
             int j = i;
             Runnable runnable = () -> {
+                Jedis jedis = new Jedis(REDIS_IP, Integer.valueOf(REDIS_PORT));
+                jedis.auth("123456");
+                JedLock redLock = new JedLock();
                 if(redLock.lock(jedis)) {
                     System.out.println(Thread.currentThread().getName() + j + ": 获得锁！");
-//                    try {
-//                        Thread.sleep(25000);
-//                    } catch (InterruptedException e) {
-//                        e.printStackTrace();
-//                    }
+                    try {
+                        Thread.sleep(2000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
                     System.out.println(Thread.currentThread().getName() + j + ": 处理完成！");
-                    redLock.unlock(jedis);
-                    System.out.println(Thread.currentThread().getName() + j + ": 释放锁！");
+                    if (redLock.unlock2(jedis)) {
+                        System.out.println(Thread.currentThread().getName() + j + ": 释放锁！");
+                    }
                 }else {
                     System.out.println("get lock fail!!!");
                 }
